69ba9f91e17a8bf417e12a1df00be98b3b837bbc
v.mishurenko <paranoiawe@gmail.com>
2024-09-24 01:29:42 +0300

    Create tests for Product, Category

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+from main import Product, Category
+import pytest
+
+@pytest.fixture
+def product1():
+    return Product("Product 1", "Description of Product 1", 1.0, 1)
+
+@pytest.fixture
+def product2():
+    return Product("Product 2", "Description of Product 2", 2.0, 2)
+
+@pytest.fixture
+def product3():
+    return Product("Product 3", "Description of Product 3", 3.0, 3)
+
+@pytest.fixture
+def category1(product1, product2, product3):
+    return Category(
+        "Category Name",
+        "Category Description",
+        [product1, product2, product3],
+    )

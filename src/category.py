ada704019f6ff6e5de145ed936d4972954befb2e
v.mishurenko <paranoiawe@gmail.com>
2024-09-28 02:29:24 +0300

    upd category.py, product.py

--- src/category.py
+++ src/category.py
@@ -5,15 +5,23 @@ class Category:
 
     name: str
     description: str
-    products: list
+    __products: list
 
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        return "".join([f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт.\n" for p in self.__products])
